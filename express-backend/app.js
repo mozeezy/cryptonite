135ab1142f5311827f0d5fca6a193b96736762a3
Mohamed Mohamed <justmoe23@gmail.com>
2022-02-13 02:07:17 +0000

    create transactions route

--- express-backend/app.js
+++ express-backend/app.js
@@ -8,6 +8,7 @@ const dbHelpers = require("./helpers/db_helpers")(db); // using the database we 
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+const transactionsRouter = require("./routes/transactions")
 
 const app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/api/users", usersRouter(dbHelpers)); // this route receives the db_helpers functions.
+app.use("/api/transactions", transactionsRouter(dbHelpers) )
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
