d12042dddc4b4ecbec966a311a608f4b1d006388
Mohamed Mohamed <justmoe23@gmail.com>
2022-02-13 01:54:16 +0000

    add constraint to buy_or_sell column in the transactions table

--- express-backend/db/schema/02_transactions.sql
+++ express-backend/db/schema/02_transactions.sql
@@ -7,7 +7,7 @@ CREATE TABLE transactions (
   id SERIAL PRIMARY KEY NOT NULL,
   amount MONEY,
   created_at TIMESTAMP,
-  buy_or_sell BOOLEAN,
+  buy_or_sell VARCHAR(255) NOT NULL CONSTRAINT buy_sell CHECK (buy_or_sell IN ('buy', 'sell')),
   coin_name VARCHAR(255) NOT NULL CONSTRAINT check_name CHECK (coin_name IN ('BTC', 'LTC', 'ETH')),
   coin_amount DECIMAL,
   user_id INTEGER REFERENCES users (id) ON DELETE CASCADE   
