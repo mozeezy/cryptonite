b56de45deda2843b222f8b05e23e5a8937ec2739
Reshawn <reshawnhamilton97@gmail.com>
2022-02-16 22:14:17 +0000

    articles rendering sucesfully no styling

--- client/src/components/DisplayArticle.jsx
+++ client/src/components/DisplayArticle.jsx
@@ -14,8 +14,8 @@ const DisplayArticle = () => {
       setIsLoading(true);
       //this is an axios instance created with the baseurl: "https://api.coingecko.com/api/v3" named "coingeckoRequest"
       const response = await axios.get("https://cryptonews-api.com/api/v1/category?section=general&items=50&token=socdthblb76bbyn0r2d72tzciiildksgzsmm5g5b");
-      console.log(response.data)
-      setArticles(response.data)
+      console.log(response.data.data)
+      setArticles(response.data.data)
       setIsLoading(false)
     }
 
