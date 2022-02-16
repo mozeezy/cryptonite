5734e723c3f530ecc23ac5434012924655bd47ec
Mohamed Mohamed <justmoe23@gmail.com>
2022-02-16 21:40:51 +0000

    finish transactions form

--- express-backend/routes/transactions.js
+++ express-backend/routes/transactions.js
@@ -6,5 +6,10 @@ module.exports = (dbHelpers) => {
   router.get("/new-transaction", (req, res) => {
     res.render("new_transaction");
   });
+
+  router.post("/", (req, res) => {
+    const { coins, action, dollarPrice } = req.body;
+    console.log("I'M REQ.BODY >>>>", req.body);
+  });
   return router;
 };
