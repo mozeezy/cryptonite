84200417bd4dbbb6b2d07c3deb79d4bb0a5661bf
Mohamed Mohamed <justmoe23@gmail.com>
2022-02-17 02:21:06 +0000

    finish the create transaction form

--- express-backend/helpers/transactions_helpers.js
+++ express-backend/helpers/transactions_helpers.js
@@ -1,5 +1,5 @@
 module.exports = (db) => {
-  const getAllTransactions = () => {
+  const getTransactions = () => {
     const query = {
       text: `SELECT * FROM transactions;`,
     };
@@ -9,8 +9,19 @@ module.exports = (db) => {
       .catch((err) => err);
   };
 
+  const addNewTransaction = (coinName, action, dollarAmount, date) => {
+    const query = {
+      text: `INSERT INTO transactions (coin_name, buy_or_sell, amount, created_at) VALUES ($1, $2, $3, $4) RETURNING *`,
+      values: [coinName, action, dollarAmount, date],
+    };
+    return db
+      .query(query)
+      .then((result) => result.rows[0])
+      .catch((err) => err);
+  };
 
   return {
-    getAllTransactions,
+    getTransactions,
+    addNewTransaction,
   };
 };
--- express-backend/routes/transactions.js
+++ express-backend/routes/transactions.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = require("../app");
 const router = express.Router();
 
-module.exports = (dbHelpers) => {
+module.exports = ({ getTransactions, addNewTransaction }) => {
   router.get("/new-transaction", (req, res) => {
     res.render("new_transaction");
   });
@@ -10,6 +10,17 @@ module.exports = (dbHelpers) => {
   router.post("/", (req, res) => {
     const { coins, action, dollarPrice } = req.body;
     console.log("I'M REQ.BODY >>>>", req.body);
+    let today = new Date();
+    const dd = String(today.getDate()).padStart(2, "0");
+    const mm = String(today.getMonth() + 1).padStart(2, "0");
+    const yyyy = today.getFullYear();
+
+    today = mm + "/" + dd + "/" + yyyy;
+
+    console.log("today is: ", today);
+    addNewTransaction(coins, action, dollarPrice, today).then((data) => {
+      console.log("I AM DATA >>>>>", data);
+    });
   });
   return router;
 };
